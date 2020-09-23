4bef9de8970177d1fdda233c27759e04ef90d5e2
himesh-dev <askhimesh1@gmail.com>
2020-09-23 23:59:18 +0530

    module rules added in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
